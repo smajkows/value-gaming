76d38f818b452059e818429feb56db42c2a5b78c
smajkows <scottm@blinkfire.com>
2020-08-27 18:25:25 -0500

    #5 wrap in try except

--- moon_landing_3/views.py
+++ moon_landing_3/views.py
@@ -564,10 +564,26 @@ class AccountPageHandler(View):
 
         json_positions = json.loads(positions)
 
-        week_gain = (one_week_balances[-1] - one_week_balances[0])/one_week_balances[0] if one_week_balances[0] else 0
-        month_gain = (one_month_balances[-1] - one_month_balances[0])/one_month_balances[0] if one_month_balances[0] else 0
-        year_gain = (one_year_balances[-1] - one_year_balances[0])/one_year_balances[0]
-        alltime_gain = (daily_stats_balances[-1] - daily_stats_balances[0])/daily_stats_balances[0]
+        try:
+            week_gain = (one_week_balances[-1] - one_week_balances[0])/one_week_balances[0] if one_week_balances[0] else 0
+        except:
+            week_gain = 0
+
+        try:
+            month_gain = (one_month_balances[-1] - one_month_balances[0])/one_month_balances[0] if one_month_balances[0] else 0
+        except:
+            month_gain = 0
+
+        try:
+            year_gain = (one_year_balances[-1] - one_year_balances[0])/one_year_balances[0]
+        except:
+            year_gain = 0
+
+        try:
+            alltime_gain = (daily_stats_balances[-1] - daily_stats_balances[0])/daily_stats_balances[0]
+        except:
+            alltime_gain = 0
+
 
         context = {'labels': daily_stats_labels, 'balances': daily_stats_balances, 'positions': json_positions,
                    'transactions': transactions, 'one_week_labels': one_week_labels, 'one_week_balances': one_week_balances,
