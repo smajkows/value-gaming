eff0d7b253ae0e4f466e3355f1e24c256ceb9685
smajkows <scottm@blinkfire.com>
2021-03-13 20:46:51 -0600

    exact path match

--- value_gaming/static/js/Base.js
+++ value_gaming/static/js/Base.js
@@ -7,6 +7,7 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import OverUnder from "./OverUnder";
+import Empty from "./Empty";
 
 class Base extends React.Component {
   render() {
@@ -25,7 +26,9 @@ class Base extends React.Component {
               </Navbar.Collapse>
             </Navbar>
             <Router>
+                <Route exact path="/" component={OverUnder} />
                 <Route path="/overunder" component={OverUnder} />
+                <Route path="/empty" component={Empty} />
             </Router>
         </React.Fragment>
 
--- value_gaming/urls.py
+++ value_gaming/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from value_gaming.views import OverUnderJSONHandler, login, OverUnderHandler
+from value_gaming.views import OverUnderJSONHandler, landing, login, OverUnderHandler
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', login),
+    path('', landing),
     path('overunder', OverUnderHandler.as_view()),
-    path('overunder_value', OverUnderJSONHandler.as_view())
+    path('overunder_value', OverUnderJSONHandler.as_view()),
+    path('empty', landing)
 ]
